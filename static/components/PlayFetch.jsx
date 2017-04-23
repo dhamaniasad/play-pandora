5c63cc45cdc0df1351fe803b82ac944de1e2933f
Asad Dhamani <dhamaniasad@gmail.com>
2017-04-24 01:52:02 +0530

    Clicker

--- static/components/PlayFetch.jsx
+++ static/components/PlayFetch.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 class PlayFetch extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			counter: 0
+		};
+
+
+		this.onClickHandler = this.onClickHandler.bind(this);
+	}
+
+	onClickHandler() {
+		this.setState({counter: this.state.counter + 1});
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Hello!</h1>
-				<Button>Click me!</Button>
+				<h3>Clicked {this.state.counter} times</h3>
+				<Button onClick={this.onClickHandler}>Click me!</Button>
 			</div>
 		)
 	}
