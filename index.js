ed193c23cfa1d4245685e0457368ef0ad484389c
Asad Dhamani <dhamaniasad@gmail.com>
2017-04-24 00:05:24 +0530

    Add nunjucks

--- index.js
+++ index.js
@@ -3,31 +3,38 @@ const express = require("express");
 const PlayMusic = require("playmusic");
 const Anesidora = require("anesidora");
 
+const nunjucks = require("nunjucks");
+
 let app = express();
 
-app.get("*", (req, res) => {
-	res.sendFile(`${__dirname}/client/index.html`);
+nunjucks.configure('views', {
+	autoescape: true,
+	express: app
 });
 
-let pandora = new Anesidora(process.env.PANDORA_ID, process.env.PANDORA_PASS);
-
-let pm = new PlayMusic();
-
-pm.init({email: process.env.GMAIL_ID, password: process.env.GMAIL_PASS}, (err) => {
-	if (err) {
-		console.error(err);
-	} else {
-		pm.getPlayLists((err, data) => {
-			pandora.login((err) => {
-				if (err) throw err;
-				pandora.request("user.getStationList", (err, stationList) => {
-					if (err) throw err;
-					console.log(stationList);
-				});
-			});
-		});
-	}
+app.get("/", (req, res) => {
+	res.render('pages/index.html');
 });
+
+// let pandora = new Anesidora(process.env.PANDORA_ID, process.env.PANDORA_PASS);
+
+// let pm = new PlayMusic();
+
+// pm.init({email: process.env.GMAIL_ID, password: process.env.GMAIL_PASS}, (err) => {
+// 	if (err) {
+// 		console.error(err);
+// 	} else {
+// 		pm.getPlayLists((err, data) => {
+// 			pandora.login((err) => {
+// 				if (err) throw err;
+// 				pandora.request("user.getStationList", (err, stationList) => {
+// 					if (err) throw err;
+// 					console.log(stationList);
+// 				});
+// 			});
+// 		});
+// 	}
+// });
 
 app.listen(3000, () => {
 	console.log("App listening on port 3000!");
